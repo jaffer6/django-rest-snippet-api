abffad6639d85083021e35ab815e7329db849431
jaffer6 <aamir@jaffer.me.uk>
2016-09-23 15:19:54 +0100

    Working

--- tutorial/snippets/admin.py
+++ tutorial/snippets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Snippet
 
-# Register your models here.
+# Register your models here
+admin.site.register(Snippet)
+
+
+class SnippetAdmin(admin.ModelAdmin):
+    pass
+    #exclude = ["highlighted",]
+    #readonly_fields=("highlighted",)
